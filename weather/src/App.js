446086b6141368d0c283e981944b0f5fff298c5a
nedim58 <toymusnedim.58@gmail.com>
2023-11-01 17:37:29 +0300

    init repo

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+
+import About from './components/About.js';
+import './App.css';
+
+
+function App() {
+return (
+    <div className="App">
+      <About/>
+    </div>
+  );
+}
+
+export default App;
